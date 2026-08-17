baa384056eba0c25e8be56592864f7f0b764c329
Aditi Sinha <aditisinha.241cs202@nitk.edu.in>
2026-08-18 01:49:41 +0530

    Create client.py

---
+++ week4/client.py
@@ -0,0 +1,27 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((HOST, PORT))
+
+print("Connected to server")
+print("Type 'exit' to disconnect")
+
+while True:
+
+    message = input("You: ")
+
+    if message.lower() == "exit":
+        break
+
+    client.sendall(message.encode())
+
+    response = client.recv(1024)
+
+    print("Server:", response.decode())
+
+client.close()
+print("Disconnected")
